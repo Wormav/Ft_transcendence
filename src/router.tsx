06711a7efd156fd69b5d28b4ab5b6698b98b55e8
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-05-07 16:40:22 +0200

    update vite config

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import Layout from './components/Layout/Layout';
+import Layout from './components/Layout/Layout.tsx';
 
 const router = createBrowserRouter([
 	{
