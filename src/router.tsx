119d01fbc29102da0082745e52cdab180c679483
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-05-07 16:56:01 +0200

    update vite config

--- src/router.tsx
+++ src/router.tsx
@@ -2,38 +2,33 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Layout from './components/Layout/Layout.tsx';
 
-const router = createBrowserRouter(
-	[
-		{
-			path: '/',
-			element: <Layout />,
-			children: [
-				{
-					path: '/',
-					element: <Home />,
-					children: [],
-				},
-				{
-					path: '/profile',
-					element: <div>Profile Page</div>,
-					children: [],
-				},
-				{
-					path: '/game',
-					element: <div>Game Page</div>,
-					children: [],
-				},
-				{
-					path: '/logout',
-					element: <div>Logout Page</div>,
-					children: [],
-				},
-			],
-		},
-	],
+const router = createBrowserRouter([
 	{
-		basename: '/Ft_transcendence',
-	}
-);
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+				children: [],
+			},
+			{
+				path: '/profile',
+				element: <div>Profile Page</div>,
+				children: [],
+			},
+			{
+				path: '/game',
+				element: <div>Game Page</div>,
+				children: [],
+			},
+			{
+				path: '/logout',
+				element: <div>Logout Page</div>,
+				children: [],
+			},
+		],
+	},
+]);
 
 export default router;
